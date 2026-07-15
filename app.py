1c42d492600248735d9274ec7004dd015ae2f830
Shaurya Garg <shaurya.garg2211@gmail.com>
2026-07-15 13:24:41 +0300

    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -716,8 +716,8 @@ def reset():
     return render_template("reset.html")
 
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
+if __name__ == "__main__":
     app.run(debug=True)
